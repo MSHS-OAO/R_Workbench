1462daa8e88ec7ea1a5301d76196a2cea66723bb
Weber <anjelica.weber@mssm.edu>
2022-02-14 12:52:54 -0500

    Fuction to determine path to share drive on R Workbench or R Studio

---
+++ Define_Root_Path_Fuction.R
@@ -0,0 +1,25 @@
+
+
+define_root_path <- function(){
+  #Check if directory is from R Workbench; starts with '/home'
+  if(grepl("^/home", dirname(getwd()))){
+    #Check if mapped Sharedrvie starts at folder Presidents or deans
+    ifelse(list.files("/SharedDrive/data/") == "Presidents",
+           #Define prefix of path to share drive with R Workbench format
+           output <- "/SharedDrive/data/Presidents/", 
+           output <- "/SharedDrive/data/deans/Presidents/")
+  }#Check if directory is from R Studio; starts with an uppercase letter than ':'
+  else if(grepl("^[[:upper:]]+:", dirname(getwd()))){
+    #Determine which drive is mapped to Sharedrive (x)
+    for(i in LETTERS){
+      if(any(grepl("deans|Presidents", list.files(paste0(i, "://"))))){x <- i}
+    }
+    #Check if mapped Sharedrvie starts at folder Presidents or deans
+    ifelse(list.files(paste0(x, "://")) == "Presidents",
+           #Define prefix of path to share drive with R Studio format
+           output <- paste0(x, ":/Presidents/"),
+           output <- paste0(x, ":/deans/Presidents/"))
+    
+  }
+  return(output)
+}
